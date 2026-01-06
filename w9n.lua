f752cec5c41946b76b33ba28b90e772253008e5e
di🥺di🤓di <xjv_2025@qq.com>
2026-01-06 20:36:06 +0800

    更新 w9n.lua

--- w9n.lua
+++ w9n.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
     Name = "UI 示例窗口",
@@ -9,19 +9,25 @@ local Window = Rayfield:CreateWindow({
     },
 })
 
+-- 创建标签页（使用冒号:）
 local 测试 = Window:CreateTab("测试")
 
+-- Rayfield中没有:section()
 测试:CreateSection("测试信息")
 
-测试:CreateLabel("欢迎使用Wan!")
+-- 创建文字标签
+测试:CreateLabel("欢迎使用Wan！")
 
+-- 显示玩家名
 测试:CreateLabel("用户名: " .. game.Players.LocalPlayer.Name)
 
+-- 显示服务器ID
 测试:CreateLabel("服务器ID: " .. game.JobId)
 
+-- 添加一个按钮试试
 测试:CreateButton({
     Name = "测试按钮",
     Callback = function()
-        print("按钮被点击!")
+        print("按钮被点击！")
     end
 })
